182bd8f8a8655a5803458fa35febcc46f48b4354
gigasquid <cmeier@gigasquidsoftware.com>
2015-10-20 10:29:24 -0400

    project details

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject stanford-talk "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Annotator for text using the Stanford CoreNLP"
+  :url "https://github.com/gigasquid/stanford-talk"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
